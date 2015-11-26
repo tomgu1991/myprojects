4d6eccd1378d2e94d438de9edc275ecf9b2ce019
tomgu1991 <guzuxing@sina.com>
2015-11-26 18:48:12 +0800

    adding MyPrinter class

---
+++ Test/src/tomgu/entity/MyPrinter.java
@@ -0,0 +1,7 @@
+package tomgu.entity;
+
+public class MyPrinter {
+	public static void print(String str){
+		System.out.println(str);
+	}
+}
---
+++ Test/src/tomgu/test/TestMyPrinter.java
@@ -0,0 +1,12 @@
+package tomgu.test;
+
+import tomgu.entity.MyPrinter;
+
+public class TestMyPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyPrinter.print(System.getenv("PATH"));
+	}
+
+}
