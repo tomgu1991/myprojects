5b5ef0f0e1dc4a26992ecf6a731cce1b8bb6ccbd
tomgu1991 <guzuxing@sina.com>
2015-12-21 09:26:59 +0800

    refactoring testIDR

--- java/ParseSingleJava/test/com/tomgu/test/detection/TestIDRDetector.java
+++ java/ParseSingleJava/test/com/tomgu/test/detection/TestIDRDetector.java
@@ -11,13 +11,30 @@ import com.tomgu.util.log.GlobalLog;
 public class TestIDRDetector {
 
 	public static void main(String[] args) {
-		//TODO get clone node pair lines from clone deteciton result
-		GlobalLog.LogProcess(GlobalProperty.GETCLONEPAIR);
+		/*
+		 * clone detection
+		CloneClass	5589
+		25007	VersionInsensitiveBugComparator.java	227	230
+		25008	VersionInsensitiveBugComparator.java	218	221
+		*/
 		String filePathRef = "./source/bug.java";
 		String filePathTar = "./source/bug.java";
-		//if line contains } remove this line
 		int[] linesRef = {218,219,220}; 
 		int[] linesTar = {227,228,229};
+		TestIDRDetector detector = new TestIDRDetector();
+		detector.IDRDetector(filePathRef,linesRef,
+				filePathTar,linesTar);	
+	}
+	
+	public void IDRDetector(String ref, int[] refLines, 
+			String tar, int[] tarLines){
+		//TODO get clone node pair lines from clone deteciton result
+		GlobalLog.LogProcess(GlobalProperty.GETCLONEPAIR);
+		String filePathRef = ref;
+		String filePathTar = tar;
+		//if line contains } remove this line
+		int[] linesRef = refLines; 
+		int[] linesTar = tarLines;
 		GlobalLog.LogClonePairInfo(filePathRef, filePathTar, linesRef, linesTar);
 		
 
@@ -38,7 +55,6 @@ public class TestIDRDetector {
 			GlobalLog.LogProcess(GlobalProperty.REPORTDETECTIONRESULT);
 			GlobalLog.LogBugResult(result);	
 		}
-		
 	}
 
 }
